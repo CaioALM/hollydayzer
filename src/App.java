b7f53bb387f7b867680c417f296723552fca85d4
Caio <caiosobreira77@gmail.com>
2023-01-13 15:31:12 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import  java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        
+    }
+}
